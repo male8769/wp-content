4012cb6d3b76aa86d03a53caa82550a8349575a7
male8769 <malene_storgaard@live.dk>
2025-09-16 14:33:20 +0200

    mother og child theme

---
+++ dacnis-child-theme/functions.php
@@ -0,0 +1,16 @@
+<?php
+add_action( 'wp_enqueue_scripts', 'child_enqueue_styles' );
+function child_enqueue_styles() {
+wp_enqueue_style(
+'parent-style',
+get_template_directory_uri() . '/style.css',
+array(),
+wp_get_theme()->parent()->get( 'Version' )
+);
+wp_enqueue_style(
+'child-style',
+get_stylesheet_uri(),
+array( 'parent-style' ),
+wp_get_theme()->get( 'Version' )
+);
+}
